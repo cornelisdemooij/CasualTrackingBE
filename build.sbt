1080600b2256816df327ef9a123e4ecde8f33799
Cornelis de Mooij <codemo@gmail.com>
2019-09-27 07:35:17 +0200

    First commit, with dependencies for basic web server.

---
+++ build.sbt
@@ -0,0 +1,17 @@
+name := "CasualTracking"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-http" % "10.1.9",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.9",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.25",
+  "com.typesafe.slick" %% "slick" % "3.3.2",
+  "com.typesafe.slick" %% "slick-hikaricp" % "3.3.2",
+  "mysql" % "mysql-connector-java" % "8.0.17",
+  "org.slf4j" % "slf4j-nop" % "1.7.28",
+  "org.scalatest" %% "scalatest" % "3.0.8" % Test,
+  "com.h2database" % "h2" % "1.4.199" % Test
+)
---
+++ src/main/scala/com/cornelisdemooij/casualtracking/WebServer.scala
@@ -0,0 +1,40 @@
+package com.cornelisdemooij.casualtracking
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives.concat
+import akka.stream.ActorMaterializer
+import com.cornelisdemooij.casualtracking.api._
+
+import scala.concurrent.{Await, ExecutionContextExecutor}
+import scala.io.StdIn
+import scala.concurrent.duration._
+
+object WebServer {
+  // Needed to run the route:
+  implicit val system: ActorSystem = ActorSystem("casual-tracking-system")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  // Needed for the binding future, to cleanly unbind and shutdown the system:
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  def main(args: Array[String]): Unit = {
+    val routes = concat(
+//      DataPointEndpoint.route,
+//      DataCollectionEndpoint.route,
+//      PlotEndpoint.route
+    )
+
+    val bindingFuture = Http().bindAndHandle(
+      routes,
+      "localhost",
+      8080
+    )
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine()  // Let it run until user presses return.
+    bindingFuture
+      .flatMap(_.unbind())  // Trigger unbinding from the port...
+      .onComplete(_ => Await.result(system.terminate(), 5.seconds))  // ...and shutdown when done.
+  }
+}
